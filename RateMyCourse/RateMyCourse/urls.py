a3b7a72918debffa2a5a5ffe65238356953077f4
mehtameet12 <mehtameet12@gmail.com>
2023-06-08 18:34:21 -0400

    Blog removed and Login added

--- RateMyCourse/RateMyCourse/urls.py
+++ RateMyCourse/RateMyCourse/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
+    path('login/', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
