3fc6923c0948ec6ad202efd0f2dde53ff53c5d64
mehtameet12 <mehtameet12@gmail.com>
2023-06-08 21:40:12 -0400

    Created a basic homepage
    home app created along with a dynamic template for the homepage.

--- RateMyCourse/RateMyCourse/settings.py
+++ RateMyCourse/RateMyCourse/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "RateMyCourse.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'login/templates')],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -110,7 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'home/static/')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
