6e064ed9a6caa9dd219975a512f16e10e3086b6d
mehtameet12 <mehtameet12@gmail.com>
2023-06-10 19:42:08 -0400

    made some changes for user authentication

--- RateMyCourse/Accounts/views.py
+++ RateMyCourse/Accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.forms.utils import ErrorList
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup(request):
@@ -11,10 +14,21 @@ def signup(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,email=email, password=password1)
-        user.save()
-        print('User Created')
-        return redirect('/')
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already taken')
+                return redirect('/')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'You already have an account with us, please Log In instead')
+                return redirect('/')
+            else:
+                user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,email=email, password=password1)
+                user.save()
+                print('User Created')
+                return redirect('/')
+        else:
+            messages.error(request, 'Passwords do not match! Please try again!')
+            return redirect('/')
     else:
         return render(request, 'index.html')
 
