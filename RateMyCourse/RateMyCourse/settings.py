9454699810c7c723042705c1a6e7e6afc5c5d215
mehtameet12 <mehtameet12@gmail.com>
2023-06-10 12:36:10 -0400

    Index page is now replaced with the one I created

--- RateMyCourse/RateMyCourse/settings.py
+++ RateMyCourse/RateMyCourse/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'home/static/')
+    os.path.join(BASE_DIR, 'static/')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
