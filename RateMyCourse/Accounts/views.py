c7ee10a0f800df7c2f968d0524bdcd6ef2f00168
mehtameet12 <mehtameet12@gmail.com>
2023-06-10 21:58:24 -0400

    restructured the signup authentication if condition.

--- RateMyCourse/Accounts/views.py
+++ RateMyCourse/Accounts/views.py
@@ -14,20 +14,19 @@ def signup(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
-        if password1 == password2:
-            if User.objects.filter(username=username).exists():
-                messages.error(request, 'Username already taken')
-                return redirect('/')
-            elif User.objects.filter(email=email).exists():
-                messages.error(request, 'You already have an account with us, please Log In instead')
-                return redirect('/')
-            else:
-                user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,email=email, password=password1)
-                user.save()
-                print('User Created')
-                return redirect('/') 
-        else:
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already taken')
+            return redirect('/')
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, 'You already have an account with us, please Log In instead')
+            return redirect('/')
+        elif password1!=password2:
             messages.error(request, 'Passwords do not match! Please try again!')
+            return redirect('/')
+        else:
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,email=email, password=password1)
+            user.save()
+            print('User Created')
             return redirect('/') 
     else:
         return render(request, 'index.html')
