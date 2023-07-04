8f1088a385b1be09af1f74e28e2c75bb71711f5f
mehtameet12 <mehtameet12@gmail.com>
2023-07-03 21:16:04 -0400

    Ratings for Card is now working!

--- RateMyCourse/Search/models.py
+++ RateMyCourse/Search/models.py
@@ -12,21 +12,20 @@ class Course(models.Model):
     def __str__(self):
         return self.course_code + " " + self.course_number + " " + self.course_name
 
-# class UserCourse(models.Model):
-#     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-#     #user = models.ForeignKey('User.User', on_delete=models.CASCADE)
-#     searched_course_code = models.CharField(max_length=10)
-#     searched_course_number = models.CharField(max_length=10)
-#     course_name = models.CharField(max_length=255)
-#     difficulty = models.IntegerField()
-#     prof_name = models.CharField(max_length=100)
-#     textbook_required = models.BooleanField()
-#     prereq = models.BooleanField()
-#     delivery = models.CharField(max_length=100)
-#     description = models.TextField()
+class CourseRating(models.Model):
+    course_code = models.CharField(max_length=10)
+    course_number = models.IntegerField()
+    course_name = models.CharField(max_length=255)
+    users = models.CharField(max_length=255)
+    prof_name = models.CharField(max_length=100)
+    textbook_required = models.BooleanField()
+    prereq = models.BooleanField()
+    delivery = models.CharField(max_length=100)
+    review = models.TextField()
+    difficulty = models.IntegerField()
 
-#     class Meta:
-#         db_table = 'Rating_course'
+    class Meta:
+        db_table = 'Rating_course'
 
-#     def __str__(self):
-#         return self.course.course_code + ' ' + self.course.course_number
+    def __str__(self):
+        return self.course.course_code + ' ' + self.course.course_number
--- RateMyCourse/Search/views.py
+++ RateMyCourse/Search/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Course
+from .models import Course, CourseRating
 
 def search_results(request):
     if request.method == 'GET':
@@ -9,6 +9,9 @@ def search_results(request):
         course_code = request.GET.get('course_code')
         # Perform the search query and retrieve the results
         results = Course.objects.filter(course_code__icontains=subject_code, course_number__icontains=course_code)
-        return render(request, 'results.html', {'results': results})
+        ratings = CourseRating.objects.filter(course_code__icontains=subject_code, course_number__icontains=course_code)
+        return render(request, 'results.html', {'results': results, 'ratings': ratings} )
     else:
         return render(request, 'results.html')
+
+
